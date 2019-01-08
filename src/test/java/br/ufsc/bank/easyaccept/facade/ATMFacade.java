1c043c0b9366d887061e4f6b036009afd23ef113
ernanics <ernani.ecs@gmail.com>
2019-01-07 22:20:44 -0200

    Update ATMFacade.java

--- src/test/java/br/ufsc/bank/easyaccept/facade/ATMFacade.java
+++ src/test/java/br/ufsc/bank/easyaccept/facade/ATMFacade.java
@@ -1,8 +1,5 @@
 package br.ufsc.bank.easyaccept.facade;
 
-import java.util.Date;
-
-import br.ufsc.model.ATM;
 import easyaccept.script.test.TestFacade;
 
 public class ATMFacade extends TestFacade{
