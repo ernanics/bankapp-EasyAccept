dcc43acb6be691c10bbb0f2a5b3816ff154e3eb4
ernanics <ernani.ecs@gmail.com>
2019-01-07 22:20:31 -0200

    Update LoanFacade.java

--- src/test/java/br/ufsc/bank/easyaccept/facade/LoanFacade.java
+++ src/test/java/br/ufsc/bank/easyaccept/facade/LoanFacade.java
@@ -1,22 +1,8 @@
 package br.ufsc.bank.easyaccept.facade;
 
-import java.math.BigDecimal;
-
-import br.ufsc.model.Loan;
 import easyaccept.script.test.TestFacade;
 
 public class LoanFacade extends TestFacade{
 	
-	
-			
-	public BigDecimal calcularValorTotalDoFinanciamentoComJuros(Double valorDesejado, Double taxaDeJurosMensal, Long numeroDeParcelas){
-		Loan loan = new Loan(valorDesejado, taxaDeJurosMensal, numeroDeParcelas);
-		return loan.getLoanAmountWithInterests();
-	}
-	
-	public BigDecimal calcularValorDasParcelasMensais(Double valorDesejado, Double taxaDeJurosMensal, Long numeroDeParcelas) {
-		Loan loan = new Loan(valorDesejado, taxaDeJurosMensal, numeroDeParcelas);
-		return loan.getValueOfMontlhyInstalments();
-	}
 	
 }
