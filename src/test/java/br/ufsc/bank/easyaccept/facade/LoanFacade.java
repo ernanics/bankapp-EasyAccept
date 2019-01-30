077d2df8cc351111cc3f6c299b2beb0f11dfbafe
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2019-01-30 11:36:23 -0200

    8 - Calcular valor das parcelas de financiamento

--- src/test/java/br/ufsc/bank/easyaccept/facade/LoanFacade.java
+++ src/test/java/br/ufsc/bank/easyaccept/facade/LoanFacade.java
@@ -1,8 +1,20 @@
 package br.ufsc.bank.easyaccept.facade;
 
+import java.math.BigDecimal;
+
+import br.ufsc.model.Loan;
 import easyaccept.script.test.TestFacade;
 
-public class LoanFacade extends TestFacade{
-	
-	
+public class LoanFacade extends TestFacade {
+
+	public BigDecimal calcularValorTotalDoFinanciamentoComJuros(Double valorDesejado, Double taxaDeJurosMensal, Long numeroDeParcelas) {
+		Loan emprestimo = new Loan(valorDesejado, taxaDeJurosMensal, numeroDeParcelas);
+		return emprestimo.getLoanAmountWithInterests();
+	}
+
+	public BigDecimal calcularValorDasParcelasMensais(Double valorDesejado, Double taxaDeJurosMensal, Long numeroDeParcelas) {
+		Loan emprestimo = new Loan(valorDesejado, taxaDeJurosMensal, numeroDeParcelas);
+		return emprestimo.getValueOfMontlhyInstalments();
+	}
+
 }
