347a48ee6105fea10bb5b70e41f9c5227626a52e
Lucas Pereira da Silva <lucas.pereira@ufsc.br>
2019-01-30 11:27:07 -0200

    7 - Sacar dinheiro – cartão bloqueado

--- src/test/java/br/ufsc/bank/easyaccept/EasyAcceptATMTestRunner.java
+++ src/test/java/br/ufsc/bank/easyaccept/EasyAcceptATMTestRunner.java
@@ -9,7 +9,7 @@ import easyaccept.EasyAcceptFacade;
 public class EasyAcceptATMTestRunner {
     public static void main(String[] args) throws Exception {
         List<String> files = new ArrayList<String>();
-        files.add("src/test/java/br/ufsc/bank/easyaccept/scripts/atm/script4.txt");
+        files.add("src/test/java/br/ufsc/bank/easyaccept/scripts/atm/script5.txt");
         ATMFacade atmFacade = new ATMFacade();
         EasyAcceptFacade eaFacade = new EasyAcceptFacade(atmFacade, files);
         eaFacade.executeTests();
